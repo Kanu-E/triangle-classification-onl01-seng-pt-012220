dab52e31c9a55ae61ae866b0e6d190c666cfc50d
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-04-01 00:43:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,20 +7,20 @@ def initialize (side_3, side_1, side_2)
   @side_1 =side_1
 end
 
-def kind
- if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+ def kind
+   if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
       raise TriangleError
     elsif side_1+side_2 <= side_3 || side_1+side_3 <= side_2 || side_2+side_3 <= side_1
       raise TriangleError
-    end
-    if side_1 == side_3 && side_3 == side_2
+   end
+   if side_1 == side_3 && side_3 == side_2
       :equilateral
       elsif side_2 != side_3 && side_2 != side_1 && side_3 != side_1
       :scalene
     else
       :isosceles
     end
-end
+ end
   
   class TriangleError < StandardError
    
